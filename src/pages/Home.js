a09baff856bb2cfd1a0484a3f4f039327ebec339
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2023-12-10 23:01:07 +0100

    Creation of Home and Projects page with the home title and the app header

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import '../assets/css/pages/Home.css';
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="Home-desc-image">
+                <div className="Home-desc">
+                    <h1>Hi, my name is ANTOINE LE CALLOCH<br/>
+                        and I welcome you to my personal website
+                    </h1>
+                </div>
+                <div className="Home-image">
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Projects.js
@@ -0,0 +1,14 @@
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <ul>
+                <il>
+                    <img/>
+                </il>
+            </ul>
+        </div>
+    );
+};
+
+export default Projects;
