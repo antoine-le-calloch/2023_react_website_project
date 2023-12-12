c4c128a484bbb5bcab4b311110c510def46f994a
Antoine Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2023-12-12 11:39:50 +0100

    Add the real diplomate texte and image and the new logo and add of the experiences page

---
+++ src/pages/Experiences.js
@@ -0,0 +1,13 @@
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <ul>
+                <il>
+                </il>
+            </ul>
+        </div>
+    );
+};
+
+export default Projects;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import '../styles/pages/Home.css';
+import InfoBand from "../components/InfoBand";
+import ScrollButton from "../components/ScrollButton";
 
 const Home = () => {
     return (
@@ -9,8 +11,33 @@ const Home = () => {
                         and I welcome you to my personal website
                     </h1>
                 </div>
-                <div className="Home-image">
+                <div>
+                    <ScrollButton/>
                 </div>
+            </div>
+            <div className="Info_band_listes">
+                <InfoBand {...{
+                    image: "/images/polytech_nice_sophia_noback.png",
+                    imageLocation: "left",
+                    diploma: "Engineering degree",
+                    speciality: "Computer Science specialty with minor in cybersecurity",
+                    location: "Polytech Nice Sophia - Côte d'Azur University",
+                    date: "2020 - 2024"}}/>
+                <InfoBand {...{
+                    image: "/images/GEII_toulouse_logo_noback.png",
+                    imageLocation: "right",
+                    diploma: "Specialised Undergraduate Technology Degree",
+                    speciality: "Electrical Engineering and Industrial Computing",
+                    location: "University Institute of Technology - Toulouse III Paul Sabatier University",
+                    date: "2018 - 2020"}}/>
+                <InfoBand {...{
+                    image: "/images/logo_stephane_hessel_noback.png",
+                    imageLocation: "left",
+                    diploma: "French general-track Baccalaureate in Mathematics and Science",
+                    speciality: "engineering science specialty (high honours)",
+                    location: "Stéphane Hessel high school, Toulouse",
+                    date: "2018"
+                }}/>
             </div>
         </div>
     );
